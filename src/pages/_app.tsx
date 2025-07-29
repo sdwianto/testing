a1de07f531c48ddaf1eb30ff84a360d09ea1550e
sdwianto <sdwianto@gmail.com>
2025-07-29 18:13:28 +0700

    fix clerk config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,11 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ClerkProvider>
+    <ClerkProvider 
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      signInUrl="/sign-in"
+      signUpUrl="/sign-up"
+    >
       <ThemeProvider defaultTheme="dark" storageKey="pos-theme">
         <div className={`${outfit.className}`}>
           {getLayout(<Component {...pageProps} />)}
